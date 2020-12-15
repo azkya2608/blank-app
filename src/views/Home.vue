f5a735df6fa906301fd26bb711a8b43f18a0c0b1
azkya2608 <azkyakhan1995@gmail.com>
2020-12-15 18:59:50 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-item>
+    
+    <ion-select value="brown">
+      <ion-select-option value="brown">Brown</ion-select-option>
+      <ion-select-option value="blonde">Blonde</ion-select-option>
+      <ion-select-option value="black">Black</ion-select-option>
+      <ion-select-option value="red">Red</ion-select-option>
+    </ion-select>
+  </ion-item>
+</template>
+
+<script>
+import { IonItem,  IonSelect, IonSelectOption } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonItem, IonSelect, IonSelectOption }
+});
+</script>
